87fe663446bbbd95a4de9b0cc5516fadfbc8877a
o2danquah <papa10058@gmail.com>
2024-07-17 14:17:39 +0000

    fixing error

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
-const dbcon = mongoose.connect("mongodb+srv://papa10058:Layzer2511@stocks.cjlx8n6.mongodb.net/?retryWrites=true&w=majority&appName=Stocks").then(()=>{
+const dbcon = async() => {
+    mongoose.connect("mongodb+srv://papa10058:Layzer2511@stocks.cjlx8n6.mongodb.net/?retryWrites=true&w=majority&appName=Stocks").then(()=>{
     console.log("Databse is running")
 }).catch((err)=>{
     console.log(err)
 })
-
+}
 module.exports = dbcon
--- backend/index.js
+++ backend/index.js
@@ -6,104 +6,109 @@ const { differenceInCalendarMonths } = require("date-fns");
 const nodeCron = require("node-cron");
 const bodyParser = require("body-parser");
 const UserModel = require("./DrugsSchema")
-// const dbconnection  = require("./")
+const db  = require("./dbconnection")
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-mongoose.connect("mongodb+srv://papa10058:Layzer2511@stocks.cjlx8n6.mongodb.net/?retryWrites=true&w=majority&appName=Stocks").then(() => {
-    console.log("Database running")
-}).catch((err) => {
-    console.log(err)
-})
 
+db();
 app.use(express.json());
 
 
 
 app.get('/', (req, res) =>{
-    res.json("Server is active")
+    res.send("Server is active")
 })
 
 
 app.post("/druginput", async (req, res) => {
-    let Drugs = new UserModel(req.body);
+    try {
+        let Drugs = new UserModel(req.body);
     let data = await Drugs.save()
     res.send(data)
     
+    } catch (error) {
+       res.status(200).json("Cannot input drug")    
+    }
 
 })
 
 
 app.get("/showdrugs", (req, res) => {
-     UserModel.find().then((Drugs) =>{
-        res.json(Drugs)
-     }).catch(err => res.json(err))
-    
-     UserModel.find().then((Drugs) => {
-        let a = [];
-        a = Drugs;
-        var myvar = a.filter((index, i) => {        
-            let myexpiry = index.DateExpiry;
-            const truncatedDate = new Date(myexpiry).toLocaleDateString('en-CA', {year: 'numeric', month: '2-digit', day: '2-digit'})
-  
-            const startDate = new Date();
- 
-            const endDate = new Date(truncatedDate);
-            return (differenceInCalendarMonths(endDate,startDate) == 1 || differenceInCalendarMonths(endDate,startDate) <=3)
+     try{
+        UserModel.find().then((Drugs) =>{
+            res.json(Drugs)
+         }).catch(err => res.json(err))
         
-        })
-        
-        function  sendmail(){
-        
-            return new Promise((resolve, reject) => {
-           
-                var transporter = nodemailer.createTransport({
-                      service: "gmail",
-                      auth: {
-                         user:"papa10058@gmail.com",
-                         pass: "sqdqdxaoximzgypv"
-                      }
-
-                })        
-                
-                
+         UserModel.find().then((Drugs) => {
+            let a = [];
+            a = Drugs;
+            var myvar = a.filter((index, i) => {        
+                let myexpiry = index.DateExpiry;
+                const truncatedDate = new Date(myexpiry).toLocaleDateString('en-CA', {year: 'numeric', month: '2-digit', day: '2-digit'})
+      
+                const startDate = new Date();
      
-                const mail_option = {
-                 from:"papa10058@gmail.com",
-                 to:"hendrixomar66@gmail.com",
-                 subject: "Drugs Expiring Soon",
-                 html: `<h3>Drug Store<h3/>
-                 <ul><h2> ${
-                   myvar.map((value) => {
-                    const myexpiry = value.DateExpiry;
-                    const truncatedDate = new Date(myexpiry).toLocaleDateString('en-CA', {year: 'numeric', month: '2-digit', day: '2-digit'})
-                    const endDate = new Date(truncatedDate);
-                    const date = new Date()
-                    console.log(value.DrugName + " "  + "expires in" + " " + differenceInCalendarMonths(endDate,date) + " " + "Months" );
-                    return (value.DrugName + " "  + "expires in" + " " + differenceInCalendarMonths(endDate,date) + " " + "Months")
-                   })
-                   }  </h2></ul>`
-                }
-                transporter.sendMail(mail_option, function (error, info) {
-                     if(error){
-                         return reject({message: "error occured"})
-                     }
-                     return resolve({message: "message sent successfully"})
-                })
-             })
-        
+                const endDate = new Date(truncatedDate);
+                return (differenceInCalendarMonths(endDate,startDate) == 1 || differenceInCalendarMonths(endDate,startDate) <=3)
+            
+            })
+            
+            function  sendmail(){
+            
+                return new Promise((resolve, reject) => {
+               
+                    var transporter = nodemailer.createTransport({
+                          service: "gmail",
+                          auth: {
+                             user:"papa10058@gmail.com",
+                             pass: "sqdqdxaoximzgypv"
+                          }
+    
+                    })        
+                    
+                    
          
-         
-        }
-
-        nodeCron.schedule("50 * * * * *", () => {
-            console.log("working")
-            sendmail();
-        })
-
-     })
+                    const mail_option = {
+                     from:"papa10058@gmail.com",
+                     to:"hendrixomar66@gmail.com",
+                     subject: "Drugs Expiring Soon",
+                     html: `<h3>Drug Store<h3/>
+                     <ul><h2> ${
+                       myvar.map((value) => {
+                        const myexpiry = value.DateExpiry;
+                        const truncatedDate = new Date(myexpiry).toLocaleDateString('en-CA', {year: 'numeric', month: '2-digit', day: '2-digit'})
+                        const endDate = new Date(truncatedDate);
+                        const date = new Date()
+                        console.log(value.DrugName + " "  + "expires in" + " " + differenceInCalendarMonths(endDate,date) + " " + "Months" );
+                        return (value.DrugName + " "  + "expires in" + " " + differenceInCalendarMonths(endDate,date) + " " + "Months")
+                       })
+                       }  </h2></ul>`
+                    }
+                    transporter.sendMail(mail_option, function (error, info) {
+                         if(error){
+                             return reject({message: "error occured"})
+                         }
+                         return resolve({message: "message sent successfully"})
+                    })
+                 })
+            
+             
+             
+            }
+    
+            nodeCron.schedule("50 * * * * *", () => {
+                console.log("working")
+                sendmail();
+            })
+    
+         })
+     }
+     catch(error){
+          res.status(200).json("Problem occured")
+     }
     
      
     
