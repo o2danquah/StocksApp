511f8cc49870f8168cb3b7e9e5f7702045e83f85
o2danquah <papa10058@gmail.com>
2024-11-14 14:56:16 +0000

    fixing 404 issue

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,15 @@ const UserRoutes = require("./Routes/routes")
 const EmployeeRoutes = require("./Routes/Employee")
 const app = express();
 const cron = require("./Controllers/cron")
-const dbconnection = require("./dbconnection")
+const dbconnection = require("./dbconnection");
+const router = require("./Routes/routes");
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 dbconnection();
 
-app.use("/users", UserRoutes)
-app.use("/employee", EmployeeRoutes)
+app.use("/", router)
 app.use("/cron", cron)
 
 
